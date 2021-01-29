0d65de260401dca3ae8c3811cabe4a776164a033
GuckTube YT <56192597+GuckTubeYT@users.noreply.github.com>
2021-01-29 09:38:41 +0700

    Update termux.sh

--- termux.sh
+++ termux.sh
@@ -1,5 +1,6 @@
-# Same like Linux, but, it use pkg (maybe... termux can use apt-get, but i just use pkg, for simple :V)
-pkg update -y && pkg upgrade -y # Update and Upgrade termux first
-pkg install -y build-essential # Install build-essential (for build .cpp and .c source code)
+# Same like Linux
+rm $PREFIX/etc/apt/sources.list && echo "deb https://termux.net stable main" >> $PREFIX/etc/apt/sources.list && apt-get update
+echo "export LD_LIBRARY_PATH=/data/data/com.termux/files/usr/lib" >> ~/.bashrc && . ~/.bashrc
+apt install -y build-essential # Install build-essential (for build .cpp and .c source code)
 g++ CPPBot.cpp enet/callbacks.c enet/compress.c enet/host.c enet/list.c enet/packet.c enet/peer.c enet/protocol.c enet/unix.c -Wall -std=c++11 -fpermissive -o gtbot #Build source code into executable
 echo "Build Successfully! you just do ./gtbot on terminal"
