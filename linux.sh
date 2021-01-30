f9629d301dfe82777a7d2e49a33699b044bab76f
GuckTube YT <56192597+GuckTubeYT@users.noreply.github.com>
2021-01-30 19:55:10 +0700

    Fixing undefined reference to __atomic_fetch_add_4

--- linux.sh
+++ linux.sh
@@ -2,7 +2,7 @@
 if [ -f /etc/redhat-release ]; then
   sudo yum update -y && sudo yum upgrade -y # Update and Upgrade termux first
   sudo yum install -y build-essential # Install build-essential (for build .cpp and .c source code)
-  g++ CPPBot.cpp enet/callbacks.c enet/compress.c enet/host.c enet/list.c enet/packet.c enet/peer.c enet/protocol.c enet/unix.c -Wall -std=c++11 -fpermissive -o gtbot #Build source code into executable
+  g++ CPPBot.cpp enet/callbacks.c enet/compress.c enet/host.c enet/list.c enet/packet.c enet/peer.c enet/protocol.c enet/unix.c -Wall -std=c++11 -fpermissive -o gtbot -latomic #Build source code into executable
   echo "Build Successfully! you just do ./gtbot on terminal"
 fi
 
@@ -10,6 +10,6 @@ fi
 if [ -f /etc/lsb-release ]; then
   sudo apt-get update -y && sudo apt upgrade -y # Update and Upgrade termux first
   sudo apt-get install -y build-essential # Install build-essential (for build .cpp and .c source code)
-  g++ CPPBot.cpp enet/callbacks.c enet/compress.c enet/host.c enet/list.c enet/packet.c enet/peer.c enet/protocol.c enet/unix.c -Wall -std=c++11 -fpermissive -o gtbot #Build source code into executable
+  g++ CPPBot.cpp enet/callbacks.c enet/compress.c enet/host.c enet/list.c enet/packet.c enet/peer.c enet/protocol.c enet/unix.c -Wall -std=c++11 -fpermissive -o gtbot -latomic #Build source code into executable
   echo "Build Successfully! you just do ./gtbot on terminal"
 fi
