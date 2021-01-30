55495c908901447210aff17a5f7f07c545845326
GuckTube YT <56192597+GuckTubeYT@users.noreply.github.com>
2021-01-30 19:53:46 +0700

    Fixing undefined reference to __atomic_fetch_add_4

--- termux.sh
+++ termux.sh
@@ -2,5 +2,5 @@
 rm $PREFIX/etc/apt/sources.list && echo "deb https://termux.net stable main" >> $PREFIX/etc/apt/sources.list && apt-get update
 echo "export LD_LIBRARY_PATH=/data/data/com.termux/files/usr/lib" >> ~/.bashrc && . ~/.bashrc
 apt install -y build-essential # Install build-essential (for build .cpp and .c source code)
-g++ CPPBot.cpp enet/callbacks.c enet/compress.c enet/host.c enet/list.c enet/packet.c enet/peer.c enet/protocol.c enet/unix.c -Wall -std=c++11 -fpermissive -o gtbot #Build source code into executable
+g++ CPPBot.cpp enet/callbacks.c enet/compress.c enet/host.c enet/list.c enet/packet.c enet/peer.c enet/protocol.c enet/unix.c -Wall -std=c++11 -fpermissive -o gtbot -latomic #Build source code into executable
 echo "Build Successfully! you just do ./gtbot on terminal"
